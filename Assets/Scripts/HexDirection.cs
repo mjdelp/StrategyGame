22a2d267f3227e169b48e821271895da532032ff
Michael Delp <michael.delp@tri.global>
2020-03-22 19:21:14 -0400

    Initial Commit
    from https://catlikecoding.com/unity/tutorials/hex-map/ Blending Cell Colors

---
+++ Assets/Scripts/HexDirection.cs
@@ -0,0 +1,18 @@
+
+public enum HexDirection {
+    NE, E, SE, SW, W, NW
+}
+
+public static class HexDirectionExtensions {
+    public static HexDirection Opposite (this HexDirection direction) {
+        return (HexDirection)(((int)direction + 3) % 6);
+    }
+    
+    public static HexDirection Previous (this HexDirection direction) {
+		return (HexDirection)(((int)(direction + 6) - 1) % 6);
+	}
+
+	public static HexDirection Next (this HexDirection direction) {
+		return (HexDirection)(((int)direction + 1) % 6);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # StrategyGame
+
+Instructions for Unity Setup
+
+Setup unity for Github, following the instructions on this page:
+https://thoughtbot.com/blog/how-to-git-with-unity
